5dd0236f601961806a787c26f8dd78233d892565
mikuta0407 <flute.takkun0407@gmail.com>
2023-12-30 23:09:15 +0900

    add checkin notification

--- checkin.php
+++ checkin.php
@@ -42,4 +42,26 @@ $res["result"] = "Created";
 header("HTTP/1.1 201 Created");
 echo json_encode($res);
 
+// DiscordWebhook組み立て
+
+$targets = "";
+for ($i = 0; $i < count($data["target"]); $i++){
+    $targets = $targets . $data["target"][$i];
+    if ($i != count($data["target"]) - 1) {
+        $targets = $targets . ", ";
+    }
+    
+}
+$discordContent["content"] = $data["visitor"] . "さんが " . $targets ." を訪れました! 時刻: ". $insertJson["date"];
+$json = json_encode($discordContent);
+
+$url = "https://discord.com/api/webhooks/1190247646972096586/NbX5wZMSYW9IIkRRFYR7lNDFBtoys0kfyqKRsU3-sMESeV3gWoFJ-FVcJrhUvfUBM9jj";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+$result=curl_exec($ch);
+curl_close($ch);
 ?>
