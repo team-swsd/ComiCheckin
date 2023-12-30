97d513981e4f142b5f0cf2ce1553836e8797acfa
mikuta0407 <flute.takkun0407@gmail.com>
2023-12-30 22:53:05 +0900

    fixup! [update]add style

--- manage/member.php
+++ manage/member.php
@@ -1,93 +1,112 @@
-<?php
-// メンバー一覧
-// 追加と削除ができる
+<!DOCTYPE html>
+<html lang="ja">
 
-require_once(dirname(__FILE__) . '/../lib/db.php');
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
+  <link rel="stylesheet" href="../common.css" />
+  <link rel="stylesheet" href="./member.css" />
 
-// 新規追加があったら
-if (isset($_POST['addName'])) {
-    try {
-        $stmt = $db->prepare('INSERT INTO member VALUES(NULL, :name)');
-        $stmt->bindValue(':name', $_POST['addName'], SQLITE3_TEXT);
-        $stmt->execute();
-    } catch (Exception $e) {
-        error_log("[insert member error!] " . $e->getMessage(), 0);
-        echo "INSERT MEMBER ERROR<br>";
-        echo "<a href =\"/\">Return to top</a>";
-        exit;
-    }
-}
+  <title>チェックイン画面 | SWSD コミケチェックインシステム</title>
+</head>
 
-if (isset($_POST['deleteId'])) {
-    try {
-        $stmt = $db->prepare('DELETE from member where id = :id');
-        $stmt->bindValue(':id', $_POST['deleteId'], SQLITE3_TEXT);
-        $stmt->execute();
-    } catch (Exception $e) {
-        error_log("[delete member error!] " . $e->getMessage(), 0);
-        echo "DELETE MEMBER ERROR<br>";
-        echo "<a href =\"/\">Return to top</a>";
-        exit;
-    }
-}
+<body>
+  <header>
+    <h1>Team SWSD ComiCheckin</h1>
+  </header>
+  <main>
+    <div class="main-container">
+      <?php
+      // メンバー一覧
+      // 追加と削除ができる
 
-//一覧出力
+      require_once(dirname(__FILE__) . '/../lib/db.php');
 
-// SELECTする
-$result = $db->query('SELECT * FROM member');
+      // 新規追加があったら
+      if (isset($_POST['addName'])) {
+        try {
+          $stmt = $db->prepare('INSERT INTO member VALUES(NULL, :name)');
+          $stmt->bindValue(':name', $_POST['addName'], SQLITE3_TEXT);
+          $stmt->execute();
+        } catch (Exception $e) {
+          error_log("[insert member error!] " . $e->getMessage(), 0);
+          echo "INSERT MEMBER ERROR<br>";
+          echo "<a href =\"/\">Return to top</a>";
+          exit;
+        }
+      }
 
-?>
-<table border="1">
-    <thead>
-        <tr>
+      if (isset($_POST['deleteId'])) {
+        try {
+          $stmt = $db->prepare('DELETE from member where id = :id');
+          $stmt->bindValue(':id', $_POST['deleteId'], SQLITE3_TEXT);
+          $stmt->execute();
+        } catch (Exception $e) {
+          error_log("[delete member error!] " . $e->getMessage(), 0);
+          echo "DELETE MEMBER ERROR<br>";
+          echo "<a href =\"/\">Return to top</a>";
+          exit;
+        }
+      }
+
+      //一覧出力
+
+      // SELECTする
+      $result = $db->query('SELECT * FROM member');
+
+      ?>
+      <table border="1">
+        <thead>
+          <tr>
             <td>ID</td>
             <td>Name</td>
             <td></td>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        if ($result) {
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($result) {
             while ($row = $result->fetchArray()) {
-                echo "<tr>\n";
-                echo "<td>" . $row["id"] . "</td>\n";
-                echo "<td>" . $row["name"] . "</td>\n";
-                echo "<td>"
-                    . "<form action=\"/manage/member.php\" method=\"POST\">\n"
-                    . "<input type=\"hidden\" name=\"deleteId\" value=\"" . $row["id"] . "\"/>\n"
-                    . "<input type=\"submit\" value=\"削除\" />\n"
-                    . "</form>"
-                    . "</td>\n";
-                echo "</tr>\n";
+              echo "<tr>\n";
+              echo "<td>" . $row["id"] . "</td>\n";
+              echo "<td>" . $row["name"] . "</td>\n";
+              echo "<td>"
+                . "<form action=\"/manage/member.php\" method=\"POST\">\n"
+                . "<input type=\"hidden\" name=\"deleteId\" value=\"" . $row["id"] . "\"/>\n"
+                . "<input class=\"delete-btn\" type=\"submit\" value=\"削除\" />\n"
+                . "</form>"
+                . "</td>\n";
+              echo "</tr>\n";
             }
-        }
+          }
 
-        ?>
-    </tbody>
-</table>
+          ?>
+        </tbody>
+      </table>
 
-<?php
+      <div>
+        <p>
+        <form action="/manage/member.php" method="POST">
+          <div>
+            <input class="name-input" placeholder="名前を入力" type="text" name="addName" id="addName" required />
+          </div>
+          <div>
+            <input class="submit-btn" type="submit" value="登録" />
+          </div>
+        </form>
+        </p>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <button onclick="linkTo(`/manage/index.php`)">管理画面へ</button>
+  </footer>
+  <script>
+    function linkTo(to) {
+      window.document.location.href = to;
+    }
+  </script>
+</body>
 
-?>
-
-<div>
-    <p>
-    <form action="/manage/member.php" method="POST">
-        <div>
-            <label for="name">Enter new member name: </label>
-            <input type="text" name="addName" id="addName" required />
-        </div>
-        <div>
-            <input type="submit" value="登録" />
-        </div>
-    </form>
-    </p>
-</div>
-<div>
-    <p>
-        <input type="button" value="Reload" onclick="var loc = window.location; window.location = loc.protocol + '//' + loc.host + loc.pathname + loc.search;" />
-    </p>
-</div>
-
-<a href="/manage/visitorlog.php">Visitor log</a><br>
-<a href ="/">Return to top</a>
+</html>
--- manage/visitorlog.php
+++ manage/visitorlog.php
@@ -1,42 +1,71 @@
-<?php
-// 来訪一覧
-// とりあえず確認のみ
+<!DOCTYPE html>
+<html lang="ja">
 
-require_once(dirname(__FILE__) . '/../lib/db.php');
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
+  <link rel="stylesheet" href="../common.css" />
+  <link rel="stylesheet" href="./visitorlog.css" />
 
-$result = $db->query('SELECT value FROM visitlog');
-?>
-<table border="1">
-    <thead>
-        <tr>
+  <title>チェックイン画面 | SWSD コミケチェックインシステム</title>
+</head>
+
+<body>
+  <header>
+    <h1>Team SWSD ComiCheckin</h1>
+  </header>
+  <main>
+    <div class="main-container">
+      <?php
+      // 来訪一覧
+      // とりあえず確認のみ
+
+      require_once(dirname(__FILE__) . '/../lib/db.php');
+
+      $result = $db->query('SELECT value FROM visitlog ORDER BY id DESC');
+      ?>
+      <table>
+        <thead>
+          <tr>
             <td>Time</td>
             <td>Name</td>
             <td>Target</td>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        if ($result) {
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($result) {
             while ($row = $result->fetchArray()) {
-                $arr = json_decode($row['value'], true);
-                echo "<tr>\n";
-                echo "<td>" . $arr["date"] . "</td>\n";
-                echo "<td>" . $arr["visitorName"] . "</td>\n";
-                echo "<td>";
-                for ($i = 0; $i < count($arr["target"]); $i++) {
-                    echo $arr["target"][$i];
-                    if ($i < count($arr["target"]) - 1) {
-                        echo ", ";
-                    }
+              $arr = json_decode($row['value'], true);
+              echo "<tr>\n";
+              echo "<td>" . $arr["date"] . "</td>\n";
+              echo "<td class=\"visitor-td\">" . $arr["visitorName"] . "</td>\n";
+              echo "<td>";
+              for ($i = 0; $i < count($arr["target"]); $i++) {
+                echo $arr["target"][$i];
+                if ($i < count($arr["target"]) - 1) {
+                  echo ", ";
                 }
+              }
 
-                echo "</td>\n";
+              echo "</td>\n";
             }
-        }
+          }
 
-        ?>
-    </tbody>
-</table>
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </main>
+  <footer>
+    <button onclick="linkTo(`/manage/index.php`)">管理画面へ</button>
+  </footer>
+  <script>
+    function linkTo(to) {
+      window.document.location.href = to;
+    }
+  </script>
+</body>
 
-<a href="/manage/member.php">Member</a><br>
-<a href ="/">Return to top</a>
+</html>
